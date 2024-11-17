3fdf4d4e1c12459c75aa7a2c0007278b9ec44734
LuanPCA <119981571+LuanPCA@users.noreply.github.com>
2024-11-17 12:41:00 -0300

    atualização

--- Assets/Art/Scripts/PlayerMovementScript.cs
+++ Assets/Art/Scripts/PlayerMovementScript.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Velocidade de movimentaÃ§Ã£o do jogador
-    private Rigidbody2D rb; // ReferÃªncia ao Rigidbody2D
+    public float moveSpeed = 5f; // Velocidade de movimentação do jogador
+    private Rigidbody2D rb; // Referência ao Rigidbody2D
 
-    private Vector2 movement; // VariÃ¡vel para armazenar a movimentaÃ§Ã£o
+    private Vector2 movement; // Variável para armazenar a movimentação
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // ObtÃ©m o componente Rigidbody2D
+        rb = GetComponent<Rigidbody2D>(); // Obtém o componente Rigidbody2D
     }
 
     void Update()
